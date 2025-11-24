171fc7ed5ab1462a75daa7b0f21b54c27cc64257
lochlancurran1 <lochlancurran33@gmail.com>
2025-11-24 22:19:11 +0000

    Refactor PlayerDetector into separate scene
    Moved PlayerDetector and its components from guard.tscn into a new player_detector.tscn scene and updated guard.tscn to instance it. Simplified player_detector.gd logic to use a single arm for cone detection and improved detection calculation. This modularizes PlayerDetector for easier reuse and maintenance.

--- ai_demo/assets/player_detector.gd
+++ ai_demo/assets/player_detector.gd
@@ -9,11 +9,17 @@ func _on_body_entered(body: Node3D) -> void:
 		
 		var my_blue_arrow = global_transform.basis.z
 		var first_arm_blue_arrow = first_arm.global_transform.basis.z
-		var second_arm_blue_arrow = second_arm.global_transform.basis.z
 		
-		var minimum_dot = my_blue_arrow.dot(first_arm_blue_arrow)
-		var maximum_dot = my_blue_arrow.dot(second_arm.global_transform.basis.z)
-		print(maximum_dot)
+		var testing_dot = my_blue_arrow.dot(first_arm_blue_arrow)
+		
+		var displacement = body.global_transform.origin - global_transform.origin
+		var direction_to_player = displacement.normalized()
+		
+		var dot_to_player = direction_to_player.dot(my_blue_arrow)
+		
+		if dot_to_player >= testing_dot:
+			print("We are Inside the cone")
+		
 	
 	
 	pass # Replace with function body.
