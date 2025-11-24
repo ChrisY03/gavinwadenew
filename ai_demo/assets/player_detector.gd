2abe2d20303f5f142c8f99ae41516a4e2ac33d14
lochlancurran1 <lochlancurran33@gmail.com>
2025-11-24 21:34:51 +0000

    Refactor PlayerDetector node hierarchy and logic
    Moved PlayerDetector and its children under the Facing node in guard.tscn, updating transforms and signal connections accordingly. Enhanced player_detector.gd to reference arm markers and calculate directional dot products for improved player detection logic.

--- ai_demo/assets/player_detector.gd
+++ ai_demo/assets/player_detector.gd
@@ -1,8 +1,23 @@
 extends Area3D
 
+@onready var first_arm = $"First Arm"
+@onready var second_arm = $"First Arm2"
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
-		print("Found the player!" + body.name)
+		print("Found the player!" + body.name, "Is " + body.name + " inside of the cone")
+		
+		var my_blue_arrow = global_transform.basis.z
+		var first_arm_blue_arrow = first_arm.global_transform.basis.z
+		var second_arm_blue_arrow = second_arm.global_transform.basis.z
+		
+		var minimum_dot = my_blue_arrow.dot(first_arm_blue_arrow)
+		var maximum_dot = my_blue_arrow.dot(second_arm.global_transform.basis.z)
+		print(maximum_dot)
+	
+	
 	pass # Replace with function body.
 		
+# func _process(delta):
+	#var player = get_tree().get_first_node_in_group("Player")
+	#var angle = global_transform.origin.signed_angle_to(player.global_tra)
