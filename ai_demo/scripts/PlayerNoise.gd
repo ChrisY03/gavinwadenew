84176e06da990696789617e3fd5bc9e64b54fe04
lochlancurran1 <lochlancurran33@gmail.com>
2025-11-25 20:34:22 +0000

    Add sprint and heartbeat sounds to PlayerNoise
    Moved audio files to a dedicated sounds directory and updated references in player.tscn and PlayerNoise.gd. Added sprint and heartbeat audio players and logic to play corresponding sounds when sprinting. Improved organization of sound assets and enhanced player feedback during sprint actions.

--- ai_demo/scripts/PlayerNoise.gd
+++ ai_demo/scripts/PlayerNoise.gd
@@ -4,20 +4,25 @@ extends Node3D
 @export var player_body: CharacterBody3D
 @export var grass_audio: AudioStreamPlayer
 @export var whistle_audio: AudioStreamPlayer
-
+@export var sprint_audio: AudioStreamPlayer
+@export var heartbeat_audio: AudioStreamPlayer
 
 var _grass_step_timer: float = 0.0
 var grass_sounds: Array = []
 var _was_on_floor: bool = false
 var _sprint_heartbeat: float = 0.0
 var whistle_sound: AudioStream
+var sprint_sound: AudioStream
+var heartbeat_sound: AudioStream
 
 func _ready():
 	grass_sounds = [
-		load("res://assets/walking-on-grass-363353.mp3")
+		load("res://assets/sounds/walking-on-grass-363353.mp3")
 	]
 	
-	whistle_sound = load("res://assets/black-ops-prop-hunt-whistle.mp3")
+	whistle_sound = load("res://assets/sounds/black-ops-prop-hunt-whistle.mp3")
+	sprint_sound = load("res://assets/sounds/running-in-wet-grass-235969.mp3")
+	heartbeat_sound = load("res://assets/sounds/heartbeat-297400.mp3")
 
 func _physics_process(delta: float) -> void:
 	if player_body == null:
@@ -29,6 +34,7 @@ func _physics_process(delta: float) -> void:
 	_was_on_floor = on_floor
 
 	var sprinting: bool = Input.is_action_pressed("sprint")
+	
 	if sprinting:
 		_sprint_heartbeat -= delta
 		if _sprint_heartbeat <= 0.0:
@@ -36,9 +42,22 @@ func _physics_process(delta: float) -> void:
 			_sprint_heartbeat = 0.4
 			print("Player sprinting")
 
-	if Input.is_action_just_pressed("whistle"):
-		Director.push_event("noise", player_body.global_transform.origin, 1.0)
-		print("Player whistled")
+		if Input.is_action_just_pressed("whistle"):
+			Director.push_event("noise", player_body.global_transform.origin, 1.0)
+			print("Player whistled")
+		
+		if sprint_audio and sprint_sound and not sprint_audio.playing:
+			sprint_audio.stream = sprint_sound
+			sprint_audio.play()
+		
+		if heartbeat_audio and heartbeat_sound and not heartbeat_audio.playing:
+			heartbeat_audio.stream = heartbeat_sound
+			heartbeat_audio.play()
+	else:
+		if sprint_audio and sprint_audio.playing:
+			sprint_audio.stop()
+		if heartbeat_audio and heartbeat_audio.playing:
+			heartbeat_audio.stop()		
 		
 		if whistle_audio and whistle_sound:
 			whistle_audio.stream = whistle_sound
